f4e6cd9adbf0f87e8b30ef9b0632f4ebe69ffeae
TheMaestro <mail@themaestro.site>
2021-10-04 02:26:00 +0000

    Change some links

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,10 @@ export default {
     Also I hate python owo`,
   email: "mailto:contact@themaestro.site",
   links: {
-    github: "https://github.com/TheMaestro0",
-    twitter: "https://twitter.com/_TheMaestroo_",
+    github: "https://github.com/TheMaestro1s",
+    twitter: "https://twitter.com/TheMaestro1s",
     kofi: "https://ko-fi.com/themaestro",
-    reddit: "https://reddit.com/u/TheMaestr00",
+    reddit: "https://reddit.com/u/TheMaestro1s",
     discord: "https://discord.gg/nNHH5apCwr"
   },
   embed: {
